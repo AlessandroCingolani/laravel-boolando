fec94a49117cbd1b8d99ec68b247fa0bceeb1914
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-11-17 15:35:25 +0100

    [ add ] container card products

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,32 +2,28 @@
 
 @section('content')
     <div class="container">
-
         <div class="product-card d-flex">
-
             <!-- box-img product -->
             <div class="col-product">
                 <div class="box-img">
-                    <a href=""><img src="#" alt="img1"></a>
-                    <a href="#"><img class="hidden" src="#" alt=""></a>
+                    <a href="#"><img src="#" alt="img1"></a>
+                    <a href="#"><img class="hidden" src="" alt=""></a>
                     <span class="heart">&hearts;</span>
                     <div class="labels">
-                        <span class="discount"></span>
+                        <span class="discount">discount</span>
                         <span class="green-piece">Sostenibilità</span>
                     </div>
                 </div>
 
                 <!-- name and price product -->
                 <div class="details-product">
-                    <h6></h6>
-                    <h4></h4>
-                    <span class="product-actual-price"></span>
-                    <span class="product-old-price"></span></span>
+                    <h6>nome</h6>
+                    <h4>modello</h4>
+                    <span class="product-actual-price">prezzo</span>
+                    <span class="product-old-price">prezzo vecchio</span>
                 </div>
             </div>
-
         </div>
 
     </div>
-    </main>
 @endsection
