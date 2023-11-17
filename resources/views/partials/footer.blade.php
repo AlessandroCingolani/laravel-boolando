8b70efd9db14778719db353d793c9eaec062451b
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-11-17 16:45:39 +0100

    [ add ] foreach for icons and links

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,20 +1,27 @@
+@php
+    $footer_menu_info = config('menues.footer_menu.info');
+    $footer_menu_social = config('menues.footer_menu.social');
+@endphp
+
 <footer>
     <div class="container">
         <div class="foot-box d-flex">
 
             <div class="information-footer">
                 <h4>Booleando s.r.l</h4>
-                {{-- <a v-for="(text,index) in info " :key="index" :href="text.href">
-                    <span>{{ text . text }}</span>
-                </a> --}}
+                @foreach ($footer_menu_info as $item)
+                    <a href="#"><span>{{ $item['text'] }}</span></a>
+                @endforeach
             </div>
 
             <nav class="footer-icons">
                 <h5>Trovaci anche su</h5>
                 <ul>
-                    {{-- <li v-for="(icon,index) in social " :key="index">
-                        <a :href="icon.href"><i :class="icon.icon"></i></a>
-                    </li> --}}
+                    @foreach ($footer_menu_social as $icon)
+                        <li>
+                            <a href="#"><i class="{{ $icon['icon'] }}"></i></a>
+                        </li>
+                    @endforeach
                 </ul>
             </nav>
         </div>
