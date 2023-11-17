4abcc172a68a32bcdb4fd2ef86153f82e6c090ed
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-11-17 19:53:02 +0100

    [ add ] array filter and error abort 404

--- routes/web.php
+++ routes/web.php
@@ -18,17 +18,39 @@ Route::get('/', function () {
 })->name('home');
 
 Route::get('/donna', function () {
-    return view('donna');
+    $products = config('products');
+    $product_donna = array_filter($products, fn ($product) => $product['gender'] === 'donna');
+    if (empty($product_donna)) {
+        abort(404);
+    }
+    return view('donna', compact('product_donna'));
 })->name('donna');
 
 Route::get('/uomo', function () {
-    return view('uomo');
+    $products = config('products');
+    $product_uomo = array_filter($products, fn ($product) => $product['gender'] === 'uomo');
+    if (empty($product_uomo)) {
+        abort(404);
+    }
+    return view('uomo', compact('product_uomo'));
 })->name('uomo');
 
 Route::get('/bambino', function () {
-    return view('bambino');
+    $products = config('products');
+    $product_bambino = array_filter($products, fn ($product) => $product['gender'] === 'bambino');
+    if (empty($product_bambino)) {
+        abort(404);
+    }
+
+    return view('bambino', compact('product_bambino'));
 })->name('bambino');
 
-Route::get('/dettagli-prodotto', function () {
-    return view('productDetail');
+Route::get('/dettagli-prodotto/{slug}', function ($slug) {
+    $products = config('products');
+    $product_array = array_filter($products, fn ($product) => $product['slug'] === $slug);
+    if (empty($product_array)) {
+        abort(404);
+    }
+    $product = $product_array[array_key_first($product_array)];
+    return view('productDetail', compact('product'));
 })->name('productDetail');
