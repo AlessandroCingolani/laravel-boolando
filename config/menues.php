47c2936754a29a782282364abfbf3459d5a29e1e
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-11-17 15:07:17 +0100

    [ init ] menues config

---
+++ config/menues.php
@@ -0,0 +1,82 @@
+<?php
+return [
+
+    "gender" => [
+        [
+            "href" => '#',
+            "text" => 'Uomo'
+        ],
+
+        [
+            "href" => '#',
+            "text" => 'Donna'
+        ],
+
+        [
+            "href" => '#',
+            "text" => 'Bambino'
+        ]
+    ],
+
+    "icons" => [
+        [
+            "href" => '#',
+            "icon" => 'fa-regular fa-user'
+        ],
+
+        [
+            "href" => '#',
+            "icon" => 'fa-regular fa-heart'
+        ],
+
+        [
+            "href" => '#',
+            "icon" => 'fa-solid fa-bag-shopping'
+        ]
+
+    ],
+
+    "info" => [
+        [
+            "href" => '#',
+            "text" => 'Informazioni legali'
+        ],
+
+        [
+            "href" => '#',
+            "text" => 'Informativa sulla privacy'
+        ],
+
+        [
+            "href" => '#',
+            "text" => 'Diritto di recesso'
+        ]
+
+    ],
+
+    "social" => [
+        [
+            "href" => '#',
+            "icon" => 'fa-brands fa-square-twitter'
+        ],
+
+        [
+            "href" => '#',
+            "icon" => 'fa-brands fa-square-facebook'
+        ],
+
+        [
+            "href" => '#',
+            "icon" => 'fa-brands fa-square-instagram'
+        ],
+        [
+            "href" => '#',
+            "icon" => 'fa-brands fa-square-pinterest'
+        ],
+        [
+            "href" => '#',
+            "icon" => 'fa-brands fa-square-youtube'
+        ]
+
+    ]
+];
