0e86f53ff3629e4cbaec27967768422ee5f17d0a
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-11-17 15:24:39 +0100

    [ add ] header and footer menues

--- config/menues.php
+++ config/menues.php
@@ -1,82 +1,87 @@
 <?php
 return [
+    "header_menu" => [
 
-    "gender" => [
-        [
-            "href" => '#',
-            "text" => 'Uomo'
+        "gender" => [
+            [
+                "href" => '#',
+                "text" => 'Uomo'
+            ],
+
+            [
+                "href" => '#',
+                "text" => 'Donna'
+            ],
+
+            [
+                "href" => '#',
+                "text" => 'Bambino'
+            ]
         ],
 
-        [
-            "href" => '#',
-            "text" => 'Donna'
-        ],
+        "icons" => [
+            [
+                "href" => '#',
+                "icon" => 'fa-regular fa-user'
+            ],
 
-        [
-            "href" => '#',
-            "text" => 'Bambino'
+            [
+                "href" => '#',
+                "icon" => 'fa-regular fa-heart'
+            ],
+
+            [
+                "href" => '#',
+                "icon" => 'fa-solid fa-bag-shopping'
+            ]
+
         ]
     ],
 
-    "icons" => [
-        [
-            "href" => '#',
-            "icon" => 'fa-regular fa-user'
+    "footer_menu" => [
+
+        "info" => [
+            [
+                "href" => '#',
+                "text" => 'Informazioni legali'
+            ],
+
+            [
+                "href" => '#',
+                "text" => 'Informativa sulla privacy'
+            ],
+
+            [
+                "href" => '#',
+                "text" => 'Diritto di recesso'
+            ]
+
         ],
 
-        [
-            "href" => '#',
-            "icon" => 'fa-regular fa-heart'
-        ],
+        "social" => [
+            [
+                "href" => '#',
+                "icon" => 'fa-brands fa-square-twitter'
+            ],
 
-        [
-            "href" => '#',
-            "icon" => 'fa-solid fa-bag-shopping'
+            [
+                "href" => '#',
+                "icon" => 'fa-brands fa-square-facebook'
+            ],
+
+            [
+                "href" => '#',
+                "icon" => 'fa-brands fa-square-instagram'
+            ],
+            [
+                "href" => '#',
+                "icon" => 'fa-brands fa-square-pinterest'
+            ],
+            [
+                "href" => '#',
+                "icon" => 'fa-brands fa-square-youtube'
+            ]
+
         ]
-
-    ],
-
-    "info" => [
-        [
-            "href" => '#',
-            "text" => 'Informazioni legali'
-        ],
-
-        [
-            "href" => '#',
-            "text" => 'Informativa sulla privacy'
-        ],
-
-        [
-            "href" => '#',
-            "text" => 'Diritto di recesso'
-        ]
-
-    ],
-
-    "social" => [
-        [
-            "href" => '#',
-            "icon" => 'fa-brands fa-square-twitter'
-        ],
-
-        [
-            "href" => '#',
-            "icon" => 'fa-brands fa-square-facebook'
-        ],
-
-        [
-            "href" => '#',
-            "icon" => 'fa-brands fa-square-instagram'
-        ],
-        [
-            "href" => '#',
-            "icon" => 'fa-brands fa-square-pinterest'
-        ],
-        [
-            "href" => '#',
-            "icon" => 'fa-brands fa-square-youtube'
-        ]
-
     ]
 ];
