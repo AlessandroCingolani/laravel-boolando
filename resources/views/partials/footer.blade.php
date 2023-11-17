234d7fa149e9c4a774d19fb54b9edde0d7e52e8e
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-11-17 14:45:52 +0100

    [ copy ] header footer from vue boolando

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,22 @@
+<footer>
+    <div class="container">
+        <div class="foot-box d-flex">
+
+            <div class="information-footer">
+                <h4>Booleando s.r.l</h4>
+                {{-- <a v-for="(text,index) in info " :key="index" :href="text.href">
+                    <span>{{ text . text }}</span>
+                </a> --}}
+            </div>
+
+            <nav class="footer-icons">
+                <h5>Trovaci anche su</h5>
+                <ul>
+                    {{-- <li v-for="(icon,index) in social " :key="index">
+                        <a :href="icon.href"><i :class="icon.icon"></i></a>
+                    </li> --}}
+                </ul>
+            </nav>
+        </div>
+    </div>
+</footer>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,25 @@
+<header>
+    <div class="top-fixed">
+        <div class="container header-bar d-flex">
+
+            <div class="gender">
+                <p>gender</p>
+                {{-- <a v-for="(type,index) in gender" :key="index" :href="type.href">{{ type . text }}</a> --}}
+            </div>
+
+            <div class="logo">
+                <a href="#"><img src="/img/boolean-logo.png" alt="logo"></a>
+            </div>
+
+            <nav class="top-icons">
+                <ul>
+                    <p>icon</p>
+                    {{-- <li v-for="(icon,index) in icons" :key="index">
+                        <a :href="icon.href"><i :class="icon.icon"></i></a>
+                    </li> --}}
+                </ul>
+            </nav>
+
+        </div>
+    </div>
+</header>
