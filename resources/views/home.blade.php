5f590453411818fe70265132cb1bf23ca835d57f
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-11-17 21:17:15 +0100

    [ add ] includes product_print

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,44 +1,10 @@
 @extends('layouts.main')
 
 @php
-    $products = config('products');
+    $product = config('products');
 @endphp
 
 
 @section('content')
-    <div class="container">
-        <!-- box-img product -->
-        <div class="product-card d-flex">
-            @foreach ($products as $item)
-                <div class="col-product">
-                    <a class="link-details" href="{{ route('productDetail', ['slug' => $item['slug']]) }}">
-                        <div class="box-img">
-                            <img src="/img/{{ $item['primaryImage'] }}" alt="img1">
-                            <img class="hidden" src="/img/{{ $item['secondaryImage'] }}" alt="">
-                            <span class="heart">&hearts;</span>
-                            <div class="labels">
-                                @if ($item['discount'] != null)
-                                    <span class="discount">{{ $item['discount'] }}</span>
-                                @endif
-                                @if ($item['sostenibilita'])
-                                    <span class="green-piece">Sostenibilità</span>
-                                @endif
-                            </div>
-                        </div>
-                    </a>
-
-                    <!-- name and price product -->
-                    <div class="details-product">
-                        <h6>{{ $item['marca'] }}</h6>
-                        <h4>{{ $item['modello'] }}</h4>
-                        <span class="product-actual-price">{{ $item['lastPrice'] }}&euro;</span>
-                        @if ($item['fullPrice'] != null)
-                            <span class="product-old-price">{{ $item['fullPrice'] }}&euro;</span>
-                        @endif
-                    </div>
-                </div>
-            @endforeach
-        </div>
-
-    </div>
+    @include('partials.product_print')
 @endsection
