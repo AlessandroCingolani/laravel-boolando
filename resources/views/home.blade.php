a03bd85e296f76c5b72a9ae58f1cc3071facfdbf
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-11-17 16:18:55 +0100

    [ add ] products loop print all details

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,28 +1,41 @@
 @extends('layouts.main')
 
+@php
+    $products = config('products');
+@endphp
+
+
 @section('content')
     <div class="container">
+        <!-- box-img product -->
         <div class="product-card d-flex">
-            <!-- box-img product -->
-            <div class="col-product">
-                <div class="box-img">
-                    <a href="#"><img src="#" alt="img1"></a>
-                    <a href="#"><img class="hidden" src="" alt=""></a>
-                    <span class="heart">&hearts;</span>
-                    <div class="labels">
-                        <span class="discount">discount</span>
-                        <span class="green-piece">Sostenibilità</span>
+            @foreach ($products as $item)
+                <div class="col-product">
+                    <div class="box-img">
+                        <a href="#"><img src="/img/{{ $item['primaryImage'] }}" alt="img1"></a>
+                        <a href="#"><img class="hidden" src="/img/{{ $item['secondaryImage'] }}" alt=""></a>
+                        <span class="heart">&hearts;</span>
+                        <div class="labels">
+                            @if ($item['discount'] != null)
+                                <span class="discount">{{ $item['discount'] }}</span>
+                            @endif
+                            @if ($item['sostenibilita'])
+                                <span class="green-piece">Sostenibilità</span>
+                            @endif
+                        </div>
+                    </div>
+
+                    <!-- name and price product -->
+                    <div class="details-product">
+                        <h6>{{ $item['marca'] }}</h6>
+                        <h4>{{ $item['modello'] }}</h4>
+                        <span class="product-actual-price">{{ $item['lastPrice'] }}&euro;</span>
+                        @if ($item['fullPrice'] != null)
+                            <span class="product-old-price">{{ $item['fullPrice'] }}&euro;</span>
+                        @endif
                     </div>
                 </div>
-
-                <!-- name and price product -->
-                <div class="details-product">
-                    <h6>nome</h6>
-                    <h4>modello</h4>
-                    <span class="product-actual-price">prezzo</span>
-                    <span class="product-old-price">prezzo vecchio</span>
-                </div>
-            </div>
+            @endforeach
         </div>
 
     </div>
