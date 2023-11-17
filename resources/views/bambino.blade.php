57a339f942ff09660d840c85e63847bd2dd31093
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-11-17 19:51:43 +0100

    [ add ] filter array for each genre

--- resources/views/bambino.blade.php
+++ resources/views/bambino.blade.php
@@ -1,8 +1,37 @@
 @extends('layouts.main');
-
-
 @section('content')
     <div class="container">
-        <h1>Sezione bambino</h1>
+
+        <div class="product-card d-flex">
+            @foreach ($product_bambino as $item)
+                <div class="col-product">
+                    <a class="link-details" href="{{ route('productDetail', ['slug' => $item['slug']]) }}">
+                        <div class="box-img">
+                            <img src="/img/{{ $item['primaryImage'] }}" alt="img1">
+                            <img class="hidden" src="/img/{{ $item['secondaryImage'] }}" alt="">
+                            <span class="heart">&hearts;</span>
+                            <div class="labels">
+                                @if ($item['discount'] != null)
+                                    <span class="discount">{{ $item['discount'] }}</span>
+                                @endif
+                                @if ($item['sostenibilita'])
+                                    <span class="green-piece">Sostenibilità</span>
+                                @endif
+                            </div>
+                        </div>
+                    </a>
+
+                    <!-- name and price product -->
+                    <div class="details-product">
+                        <h6>{{ $item['marca'] }}</h6>
+                        <h4>{{ $item['modello'] }}</h4>
+                        <span class="product-actual-price">{{ $item['lastPrice'] }}&euro;</span>
+                        @if ($item['fullPrice'] != null)
+                            <span class="product-old-price">{{ $item['fullPrice'] }}&euro;</span>
+                        @endif
+                    </div>
+                </div>
+            @endforeach
+        </div>
     </div>
 @endsection
