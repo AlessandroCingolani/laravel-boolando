3528ccbae39b2d559c4127b537cfb6113b207c3f
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-11-17 16:44:42 +0100

    [ init ] pages donna bambino uomo

---
+++ resources/views/donna.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.main');
+
+
+@section('content')
+    <div class="container">
+        <h1>Sezione donna</h1>
+    </div>
+@endsection
---
+++ resources/views/uomo.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.main');
+
+
+@section('content')
+    <div class="container">
+        <h1>Sezione uomo</h1>
+    </div>
+@endsection
